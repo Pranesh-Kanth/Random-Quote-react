a6fbf8e2537fdc7085e73cdcdcb1b8719dc9363d
Pranesh-Kanth <727721euec112@skcet.ac.in>
2023-04-20 21:02:40 +0530

    Quote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "./components/Card";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
-function App() {
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: "100vh",
+    background: "#f1f1f1",
+    paddingTop: "64px", /* Add padding to account for AppBar height */
+  },
+  heading: {
+    fontFamily: "times, Times New Roman, times-roman, georgia, serif",
+    fontSize: "25px",
+    lineHeight: "40px",
+    letterSpacing: "-1px",
+    color: "#444",
+    fontWeight: "100",
+  },
+  footer: {
+    backgroundColor: "black",
+    color: "white",
+    textAlign: "center",
+    width: "100%",
+    height: "60px",
+    position: "absolute",
+    bottom: 0,
+  },
+}));
+
+function Footer() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.footer}>
+      <Typography variant="subtitle1" align="center">
+        Thank You For Visiting!!
+      </Typography>
     </div>
   );
 }
 
-export default App;
+export default function NestedGrid() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar position="fixed" style={{ backgroundColor: "black" }}>
+        <Toolbar style={{ justifyContent: "center" }}>
+          <Typography variant="h6" className={classes.title}>
+            Random Quote Generator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        className={classes.root}
+      >
+        <Grid item md={8} sm={8} xs={10}>
+          <Card />
+        </Grid>
+      </Grid>
+      <Footer />
+    </div>
+  );
+}
